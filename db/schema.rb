b67683b862f0ef1f068ca1b10b37b5b316a7c425
YenTung Chen <cyen.tun@gmail.com>
2023-05-30 22:58:38 +0800

    push my progress -> finished faker generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_143027) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_143750) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "restaurants", force: :cascade do |t|
     t.string "title"
     t.string "content"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  Article.create!(
+    title: Faker::Sports::Football.team, #=> "Manchester United"
+    content: Faker::Sports::Football.player #=> "Lionel Messi"
+  )
+end
